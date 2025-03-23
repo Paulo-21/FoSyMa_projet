437d8acc135417a5b21764b2b59c0d0c2573db99
Paulo-21 <paul.cibier@gmail.com>
2025-03-23 16:50:14 +0100

    iug

---
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/EfficiShareMap.java
@@ -0,0 +1,24 @@
+package eu.su.mas.dedaleEtu.mas.behaviours;
+
+import jade.core.behaviours.SimpleBehaviour;
+
+public class EfficiShareMap extends SimpleBehaviour {
+
+	private static final long serialVersionUID = 7777415158602472498L;
+
+	public EfficiShareMap() {
+		
+	}
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
