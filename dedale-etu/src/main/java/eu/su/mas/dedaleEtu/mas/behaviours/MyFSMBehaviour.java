c62be1238692d1ef1d6182e2bde07401399400dd
Paulo-21 <paul.cibier@gmail.com>
2025-04-01 11:29:42 +0200

    New shareMap fixed

--- dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/MyFSMBehaviour.java
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/MyFSMBehaviour.java
@@ -122,6 +122,7 @@ public class MyFSMBehaviour extends FSMBehaviour {
                     boolean isNewNode = theMap.addNewNode(accessibleNode.getLocationId());
                     if (isNewNode) {
                     	StringBuilder builder = new StringBuilder("E");
+                    	
                     	builder.append(myPosition);
                     	builder.append(" ");
                     	builder.append(accessibleNode);
@@ -174,14 +175,11 @@ public class MyFSMBehaviour extends FSMBehaviour {
             	AID id = msgr.getSender();
             	// Processing of the message
             	String textMessage = msgr.getContent();
-            	/*if (!textMessage.startsWith("M")) {
-            		break;
-            	}*/
             	if (this.last_talk_knowlege.get(id.getLocalName()) == this.knowledge.size()) { 
             		continue;
             	}
-            	this.parse_and_learnknowlege(textMessage);
-            	String diff_knowledge = this.get_unknow_knowledge(id.getLocalName());
+            	MyFSMBehaviour.parse_and_learnknowlege(textMessage, this.myMap, this.ressources);
+            	String diff_knowledge = MyFSMBehaviour.get_unknow_knowledge(theMap, id.getLocalName(), this.knowledge, this.last_talk_knowlege);
             	ACLMessage msgrespond = new ACLMessage(ACLMessage.INFORM);
             	msgrespond.setProtocol("ShareMap");
             	msgrespond.setSender(this.myAgent.getAID());
@@ -204,12 +202,12 @@ public class MyFSMBehaviour extends FSMBehaviour {
             		break;
             	}
             	String textMessage = msgr.getContent();
-            	this.parse_and_learnknowlege(textMessage);
+            	MyFSMBehaviour.parse_and_learnknowlege(textMessage, this.myMap, this.ressources);
             	
             }
             exitValue = 2;
         }
-        private String get_unknow_knowledge(String agentname) {
+        /*private String get_unknow_knowledge(String agentname) {
         	StringBuilder builder = new StringBuilder();
         	int i = this.knowledge.size();
         	int last = this.last_talk_knowlege.get(agentname);
@@ -252,7 +250,7 @@ public class MyFSMBehaviour extends FSMBehaviour {
         		  }
         	  }
         	}
-        }
+        }*/
         @Override
         public int onEnd() {
             return this.exitValue;
@@ -321,11 +319,11 @@ public class MyFSMBehaviour extends FSMBehaviour {
                     }
                 }
                 // 3️° Check if the exploration is complete
-                /*if (!theMap.hasOpenNode()) {
+                if (!theMap.hasOpenNode()) {
                     System.out.println(this.myAgent.getLocalName() + " - Exploration successfully completed.");
                     myAgent.doDelete();
                     return;
-                }*/
+                }
                 // 4️⃣ Select the next node to move to
                 if (nextNodeId == null) {
                     // No directly accessible unexplored node, compute the shortest path to the closest open node
@@ -349,7 +347,7 @@ public class MyFSMBehaviour extends FSMBehaviour {
                 	msgrespond.setProtocol("ShareMap");
                 	msgrespond.setSender(this.myAgent.getAID());
             		msgrespond.addReceiver(id);
-            		String sendtext = get_unknow_knowledge(id.getLocalName(), this.knowledge, this.last_talk_knowlege);
+            		String sendtext = get_unknow_knowledge(theMap, id.getLocalName(), this.knowledge, this.last_talk_knowlege);
             		msgrespond.setContent(sendtext);
             		((AbstractDedaleAgent)this.myAgent).sendMessage(msgrespond);
                 }
@@ -377,7 +375,8 @@ public class MyFSMBehaviour extends FSMBehaviour {
             return exitValue;
         }
     }
-    private String get_unknow_knowledge(String agentname, ArrayList<String> knowledge, HashMap<String, Integer> last_talk_knowlege) {
+    private static String get_unknow_knowledge(MapRepresentation myMap, String agentname, ArrayList<String> knowledge, HashMap<String, Integer> last_talk_knowlege) {
+    	List<String> openodes = myMap.getOpenNodes();
     	StringBuilder builder = new StringBuilder();
     	int i = knowledge.size();
     	int last = last_talk_knowlege.get(agentname);
@@ -385,38 +384,56 @@ public class MyFSMBehaviour extends FSMBehaviour {
     		if (i == last) {
     			break;
     		}
-    		builder.append(k);
+    		String sub = k.substring(1);
+    		String[] spl = sub.split(" ");
+    		builder.append("E");
+    		if (openodes.contains(spl[0]) ) { builder.append("O"); } 
+    		else { builder.append("C"); }
+    		builder.append(spl[0]);
+    		builder.append(" ");
+			if (openodes.contains(spl[1]) ) { builder.append("O"); }
+			else { builder.append("C"); }
+			builder.append(spl[1]);
+			
     		builder.append(',');
     		i--;
     	}
     	last_talk_knowlege.put(agentname, i);
     	return builder.toString();
     }
-    private void parse_and_learnknowlege(String sharemap_result, SharedMapRepresentation myMap) {
+    private static void parse_and_learnknowlege(String sharemap_result, SharedMapRepresentation myMap, HashMap<String, HashMap<String, Integer>> ressource_value) {
     	MapRepresentation theMap = myMap.getMyMap();
     	String[] myArray = sharemap_result.split(",");
     	for (String s : myArray) {
     	  if (s.charAt(0) == 'E') {
     		  String e = s.substring(1);
     		  String[] slp = e.split(" ");
-    		  theMap.addNode(slp[0], MapRepresentation.MapAttribute.closed);
-    		  theMap.addNode(slp[1], MapRepresentation.MapAttribute.closed);
-    		  theMap.addEdge(slp[0], slp[1]);
+    		  char openstate1 = slp[0].charAt(0);
+    		  char openstate2 = slp[1].charAt(0);
+    		  MapRepresentation.MapAttribute node_attri1 = openstate1 == 'O' ? MapRepresentation.MapAttribute.open : MapRepresentation.MapAttribute.closed;
+    		  MapRepresentation.MapAttribute node_attri2 = openstate2 == 'O' ? MapRepresentation.MapAttribute.open : MapRepresentation.MapAttribute.closed;
+
+    		  String node1 = slp[0].substring(1);
+    		  String node2 = slp[1].substring(1);
+    		  
+    		  theMap.addNode(node1, node_attri1);
+    		  theMap.addNode(node2, node_attri2);
+    		  theMap.addEdge(node1, node2);
     	  }
     	  else if (s.charAt(0) == 'G') {
     		  String[] spl = s.substring(1).split(" ");
     		  String location = spl[0];
     		  String value = spl[1];
-    		  if (ressources.get("Gold").get(location) != null && ressources.get("Gold").get(location) > Integer.parseInt(value)) {
-    			  ressources.get("Gold").put(location, Integer.parseInt(value));
+    		  if (ressource_value.get("Gold").get(location) != null && ressource_value.get("Gold").get(location) > Integer.parseInt(value)) {
+    			  ressource_value.get("Gold").put(location, Integer.parseInt(value));
     		  }
     	  }
     	  else if (s.charAt(0) == 'D') {
     		  String[] spl = s.substring(1).split(" ");
     		  String location = spl[0];
     		  String value = spl[1];
-    		  if (ressources.get("Diamond").get(location) != null && ressources.get("Diamond").get(location) > Integer.parseInt(value)) {
-    			  ressources.get("Diamond").put(location, Integer.parseInt(value));
+    		  if (ressource_value.get("Diamond").get(location) != null && ressource_value.get("Diamond").get(location) > Integer.parseInt(value)) {
+    			  ressource_value.get("Diamond").put(location, Integer.parseInt(value));
     		  }
     	  }
     	}
